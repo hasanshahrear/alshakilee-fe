f44270f717d6793bda42b883f2741fdf9d5ceea0
hasanshahrear <hasanshahrear143@gmail.com>
2024-12-13 11:17:45 +0600

    modify: siderbar url

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type TProps = {
   children: React.ReactNode;
 };
@@ -10,7 +12,11 @@ export default function DashboardLayout({ children }: Readonly<TProps>) {
       </div>
       <div className="m-4 flex gap-4">
         <div className="h-[calc(100vh-8rem)] min-w-[300px] rounded-lg p-4 shadow-md">
-          layout
+          <div className="flex flex-col">
+            <Link href="/dashboard/buttom-type">Buttom Type</Link>
+            <Link href="/dashboard/design-type">Design Type</Link>
+            <Link href="/dashboard/customer">Customer Type</Link>
+          </div>
         </div>
         <div className="w-full p-4 pt-2">{children}</div>
       </div>
