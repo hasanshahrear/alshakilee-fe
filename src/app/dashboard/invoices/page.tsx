0ad03366865d51ca83357019e5950d3c251a8642
hasanshahrear <hasanshahrear143@gmail.com>
2025-05-25 15:08:03 +0600

    remove the ;

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 export default function InvoicesPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Invoices />;
+      <Invoices />
     </Suspense>
   );
 }
