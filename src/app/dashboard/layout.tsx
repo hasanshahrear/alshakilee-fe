1efcc3c214ff766f5fffd15568b9bf89e8b8e779
hasanshahrear <hasanshahrear143@gmail.com>
2025-07-23 18:05:54 +0600

    fix: logout url

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -39,7 +39,7 @@ const Sidebar = () => {
   const { data } = useSession();
   const handleLogout = async () => {
     await signOut().then(() => {
-      window.location.href = "/login";
+      window.location.href = "/api/auth/signin";
     });
   };
   return (
